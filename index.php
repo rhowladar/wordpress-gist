800db4c132322d8bf5aeac0e8644adedd2b29e4d
Rubel Howladar <rhowladar12@gmail.com>
2016-02-03 11:12:27 +0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php language_attributes(); ?>
+
+<?php bloginfo( 'charset' ); ?>
+
+<?php wp_title();?>
+
+<?php wp_head();?>
+
+<?php wp_footer():?>
+
+<?php echo get_template_directory_uri()?>/img/
+
+//for redirect home
+
+<?php site_url();?>
